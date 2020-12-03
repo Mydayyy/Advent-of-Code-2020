d7e1c2b93ea55a57e73bdda57f085c1dd5a334b7
Mydayyy <mydayyy@mydayyy.eu>
2020-12-04 00:29:34 +0100

    Day 3

---
+++ 3/src/main.rs
@@ -0,0 +1,36 @@
+use std::io::{self, prelude::*, BufReader};
+use std::fs::File;
+
+fn slope_solver(input: &Vec<String>, right: usize, down: usize) -> u32 {
+    let mut col = right;
+    let mut row = down;
+
+    let mut trees = 0;
+    while row < input.len() {
+        if *input[row].as_bytes().get(col % (input[row].len())).unwrap() == b'#' {
+            trees += 1;
+        }
+
+
+        col += right;
+        row += down;
+    }
+
+    return trees;
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let file = File::open("input")?;
+    let reader = BufReader::new(file);
+
+    let input: Vec<String> = reader.lines().filter_map(io::Result::ok).collect();
+
+    let trees = slope_solver(&input, 3, 1);
+    println!("Part 1: {}", trees);
+
+    let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let trees = slopes.iter().map(|e| { slope_solver(&input, e.0, e.1) }).fold(1, |a, v| a * v);
+    println!("Part 2: {}", trees);
+
+    Ok(())
+}
